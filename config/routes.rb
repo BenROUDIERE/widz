6e81e7ae890e9895762571974811383699ff167a
Benjamin ROUDIERE <80220859+BenROUDIERE@users.noreply.github.com>
2021-07-20 17:06:25 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+
+  devise_for :users
+
+  # catalogue
+  resources :plants, only: [:index, :show]
+  
+  resources :balconies, only: [:new, :create, :show] do
+    resources :balcony_plants, only: [] do
+      collection do
+        post :batch_create # cf tuto batch delete stimulus
+      end
+    end
+  end
+
+  resources :balcony_plants, only: [:show] do
+    resources :tasks, only: [:new, :create]
+  end
+
+  resources :tasks, only: [:index] do
+    member do
+      patch :complete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
