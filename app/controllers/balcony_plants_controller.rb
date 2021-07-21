44ee420e499d7725dd27f176d22dab297c17e040
Benjamin ROUDIERE <80220859+BenROUDIERE@users.noreply.github.com>
2021-07-21 16:07:41 +0200

    show plant

--- app/controllers/balcony_plants_controller.rb
+++ app/controllers/balcony_plants_controller.rb
@@ -1,4 +1,7 @@
 class BalconyPlantsController < ApplicationController
+  before_action :set_balcony_plants, only: [:show]
+  
   def show
   end
+
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,7 +2,7 @@ class Plant < ApplicationRecord
   has_many :balcony_plants
   has_one_attached :photo
   LUMINOSITIES = ['ombre', 'peu ensoleillé', 'ensoleillé']
-  CATEGORIES = ['Comestible', 'Perennial', 'Fruits', 'Shrub']
+  CATEGORIES = ['Comestible', 'Vivace', 'Fruits', 'Arbuste']
   validates :luminosity, presence: true, inclusion: { in: LUMINOSITIES }
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   def combative_plants
