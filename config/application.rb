ac0c4677ae03b80b951874d27f20bd7d700fe0c0
Alexandre Coulon <viabiliz@gmail.com>
2021-07-27 06:36:44 +0200

    header and 404 500

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.exceptions_app = self.routes
 module Widz
   class Application < Rails::Application
     config.generators do |generate|
