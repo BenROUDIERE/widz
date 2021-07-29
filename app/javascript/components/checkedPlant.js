bf9519a2d1962f5815e26738f4d35f3bdd1b75c5
Benjamin ROUDIERE <80220859+BenROUDIERE@users.noreply.github.com>
2021-07-29 14:51:08 +0200

    add ajax on tasks

--- app/javascript/components/checkedPlant.js
+++ app/javascript/components/checkedPlant.js
@@ -1,8 +1,9 @@
 const checkedPlant = () => {
     $(document).on("change", "input[type='checkbox']", function () {
 	    // $(this).closest('.plant')[this.checked ? "addClass" : "removeClass"]("checked");
-        $(this).closest('div > .card-plant')[this.checked ? "addClass" : "removeClass"]("checked");
+        $(this).closest('div > #tag-task')[this.checked ? "addClass" : "removeClass"]("checked");
     });
+    
 }
 
 export { checkedPlant };
