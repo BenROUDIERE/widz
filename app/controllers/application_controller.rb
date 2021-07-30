b096fa924e55d0932b9428836c6d36de4d5c3b72
Alexandre Coulon <viabiliz@gmail.com>
2021-07-30 06:37:15 +0200

    facebook og

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   helper_method :today_number_of_tasks
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   def today_number_of_tasks
     return 0 unless user_signed_in? && current_user.balcony
 
